d8d36835cd7e3033c903f3806832ddde1fce179d
mkRustam <mk.rustam96@gmail.com>
2025-07-08 22:03:37 +0300

    Обновление структуры

--- src/model/Gamer.py
+++ src/model/Gamer.py
@@ -1,14 +1,13 @@
 class Gamer:
     """Сущность игрока"""
 
-    def __init__(self, id: int, gamer_name, nickname, status):
+    def __init__(self, id: int, gamer_name, nickname):
         self.id = id
         self.gamer_name = gamer_name
         self.nickname = nickname
-        self.status = status
 
     def __repr__(self):
-        return f'Gamer({self.id}, {self.gamer_name}, {self.nickname}, {self.status})'
+        return f'Gamer({self.id}, {self.gamer_name}, {self.nickname})'
 
 def create_common_gamer(new_id, name) -> Gamer:
-    return Gamer(id=new_id, gamer_name=name, nickname=name, status="user")
+    return Gamer(id=new_id, gamer_name=name, nickname=name)
