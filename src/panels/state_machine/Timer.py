c4e9fdcdcacffaf77b110a834101610533ef10a5
Shamsudin <shamsudin@mail.ru>
2025-09-16 20:17:38 +0300

    Update

--- src/panels/state_machine/Timer.py
+++ src/panels/state_machine/Timer.py
@@ -4,7 +4,7 @@ import time
 class Timer:
     def __init__(self):
         self.last_activity_time = time.time()
-        self.timeout_seconds = 10
+        self._timeout_seconds = 10
 
     def new_time(self):
         self.last_activity_time = time.time()
@@ -12,4 +12,4 @@ class Timer:
     def time_has_passed(self):
         current_time = time.time()
         elapsed_time = current_time - self.last_activity_time
-        return elapsed_time > self.timeout_seconds
+        return elapsed_time > self._timeout_seconds
